ec4f7af282f1d5aa689df4e6581ee69ff4683c93
JohnTsai960720 <80560777+JohnTsai960720@users.noreply.github.com>
2021-03-13 11:44:59 +0800

    Add files via upload

---
+++ 01.pi.py
@@ -0,0 +1,3 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+mc.player.setTilePot(185,100,255)
---
+++ 01.py
@@ -0,0 +1,6 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+x=300
+y=150
+z=52
+mc.player.setTilePos(x,y,z)
